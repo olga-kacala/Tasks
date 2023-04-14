9031bd47dc944d4c4c12c3297b5dbd4c6eae4000
olga-kacala <olga.kacala@gmail.com>
2023-04-14 15:02:32 +0200

    Numbers task done

--- my-app/src/components/Letters.tsx
+++ my-app/src/components/Letters.tsx
@@ -1,18 +1,55 @@
 import React from "react";
 
 export const Letters = (): JSX.Element => {
-  const sentence = "this is a sentence with big first letters";
-
-  let newArr = sentence
+const sentence = "this is a sentence with big first letters";
+let newArr = sentence
     .split(" ")
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(" ");
 
   return (
     <div>
+      <h1>Result of simple JS task (letters):</h1>
       <p>{newArr}</p>
     </div>
   );
 };
 
 // In this code, we are using the + operator to concatenate the capitalized first letter of each word with the rest of the word, and then joining the words back into a sentence using the join() method with a space character as the separator. We then return a JSX element containing a p tag that displays the capitalized sentence.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- my-app/src/components/Numbers.tsx
+++ my-app/src/components/Numbers.tsx
@@ -1,10 +1,57 @@
 import React from "react";
 
 export const Numbers = ():JSX.Element => {
-const numbers = [2,2,3,7,8,7];
+const numbers = [1,3,11,11,5,7];
+const numberCountMap: { [key: string]: { count: number, index?: number } } = {};
+let maxFreq = 1;
+let mostFrequentNums: number[] = [];
 
+numbers.forEach((num, index)=>{
+    if(numberCountMap[num]) {
+        numberCountMap[num].count++;
+
+    if (numberCountMap[num].count > maxFreq) {
+        maxFreq= numberCountMap[num].count;
+        mostFrequentNums = [num];
+    } else if (numberCountMap[num].count === maxFreq) {
+        mostFrequentNums.push(num);
+    }
+    } else {
+        numberCountMap[num] = {count:1, index};
+    }
+})
+const result = mostFrequentNums[0] || null;
+console.log(result);
 
     return (
-
+<div>
+    <h1>Result of medium task in JS (numbers):</h1>
+    <p>{result !== null ? result : -1}</p>
+    </div>
     )
+    
 }
+
+// function ARR(arr) {
+//     const freqMap = {};
+//     let maxFreq = 1;
+//     let mostFreqNums = [];
+  
+//     arr.forEach((num, index) => {
+//       if (freqMap[num]) {
+//         freqMap[num].count++;
+//         if (freqMap[num].count > maxFreq) {
+//           maxFreq = freqMap[num].count;
+//           mostFreqNums = [num];
+//         } else if (freqMap[num].count === maxFreq) {
+//           mostFreqNums.push(num);
+//         }
+//       } else {
+//         freqMap[num] = { count: 1, index };
+//       }
+//     });
+  
+//     const firstMode = mostFreqNums[0] || null;
+  
+//     return firstMode !== null ? firstMode : -1;
+//   }
