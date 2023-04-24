cbd38788a723a062f3e4ccaed4572949ce8c8b76
olga-kacala <olga.kacala@gmail.com>
2023-04-24 17:53:44 +0200

    Task added

--- my-app/src/components/Loop.tsx
+++ my-app/src/components/Loop.tsx
@@ -1,6 +1,25 @@
 
 export const Loop = (): JSX.Element => {
-    for (let i=0; i<3; i++){setTimeout(function() {alert(i);},1000+1)};
+    // for (let i=0; i<3; i++){setTimeout(function() {alert(i);},1000+1)};
+
+    // for (let i=0; i<=10;i++){
+    //   if (i %2 == 0){
+    //     console.log([i])
+    //   }
+    // }
+
+// function sumArr (arr) {
+//   let sum = 0;
+// for(let i=0; i<arr.length; i++){
+//   sum =+ arr[i]
+// }
+// }
+
+let a = 1;
+let b = ++a;
+let c = a++;
+
+console.log(a,b,c)
 
     return (
       <div>
