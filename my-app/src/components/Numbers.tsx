9db2a3297a7c9b91e8355506ad647926c29661ea
olga-kacala <olga.kacala@gmail.com>
2023-06-13 12:02:11 +0200

    Pro tips

--- my-app/src/components/Numbers.tsx
+++ my-app/src/components/Numbers.tsx
@@ -5,7 +5,7 @@ const numbers = [0,0,3,3,11,11,5,7];
 
 const numberCountMap: { [key: string]: { count: number, index?: number } } = {};
 let maxFreq = 1;
-let mostFrequentNums: number[] = [];
+let mostFrequentNumb: number[] = [];
 
 numbers.forEach((num, index)=>{
     if(numberCountMap[num]) {
@@ -13,18 +13,18 @@ numbers.forEach((num, index)=>{
 
     if (numberCountMap[num].count > maxFreq) {
         maxFreq= numberCountMap[num].count;
-        mostFrequentNums = [num];
+        mostFrequentNumb = [num];
     } else if (numberCountMap[num].count === maxFreq) {
-        mostFrequentNums.push(num);
+        mostFrequentNumb.push(num);
     }
     } else {
         numberCountMap[num] = {count:1, index};
     }
 })
-// const result = mostFrequentNums[0] || null;
+// const result = mostFrequentNumb[0] || null;
 // not working with 0 
 
-const result = mostFrequentNums.includes(0) ? 0 : mostFrequentNums[0] || null;
+const result = mostFrequentNumb.includes(0) ? 0 : mostFrequentNumb[0] || null;
 
 
   
